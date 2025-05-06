57a03d0b81168ce036e52ce53a5e27b360ad42df
TATSAT28 <tatsat28@gmail.com>
2025-05-06 11:24:39 +0530

    Update 2_exercies.c

--- 2_exercies.c
+++ 2_exercies.c
@@ -2,39 +2,29 @@
 1)  write a program to findout whether given year is millennium year or not. using simple decision making statements. using simple decision making statements.
 
 
- 1 =  creat variable of the year
- 2 =  plz enter your year
- 3 =  millennium year = year/1000
- 3 = creat logic for saying  millennium year or no
- 4 = print answer.
- 5 = creat logic for saying  millennium year or not.
- 6 = print answer.
-
-*/
-
-    #include<stdio.h>
+ 
+ 
+     step2 
+     1) create variable year and accept input from user 
+     2) check whether particular year is millennium year or not
+         if year is divisble by 1000 then 
+             display message year is millennium year
+         if year is not divisble by 1000 then 
+             display message year is not millennium year
+ */
+#include<stdio.h>
 void main()
 {
-    int millennium,year;
-
-  
-    printf("\n plz Let's know about millennium year.");
-
-    printf("\nplz Enter your current year. :- ");
+    int year;
+    printf("Enter year");
     scanf("%d",&year);
-
-    millennium = year  / 1000;
-
-    if(year<1000)
+    if(year%1000==0)
     {
-        printf("\nplz Enter a viled year.try again plz.");
+        printf("year is millennium year");
     }
 
-    if(year>1000)
+    if(year%1000!=0)
     {
-        millennium = year / 1000;
-        printf("\nyear = %d",millennium);
+        printf("year is nor millennium year");
     }
-
-    printf("\nGood boy");
-}
+}   
