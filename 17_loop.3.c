aa7abae123e11a02caedc040a45e9b88af099146
TATSAT28 <tatsat28@gmail.com>
2025-05-13 13:05:36 +0530

    Add files via upload

---
+++ 17_loop.3.c
@@ -0,0 +1,24 @@
+/*
+1.2) 1 4 9 16 25  ...... 1000 
+1 creat varibal number
+2 dipllay number
+3 number = number * number
+4 print nuber
+*/
+#include<stdio.h>
+void main ()
+{
+    int number=0,sq;
+    while (number<=100)
+    { 
+        number = number+1;
+        
+        sq = number * number;
+        printf("%d ",sq);
+    }
+    
+  printf("\nnice try");
+    
+    
+
+}
