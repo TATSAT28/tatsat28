c6856be6a4738516d62c132f56e115d36edef1ba
TATSAT28 <tatsat28@gmail.com>
2025-05-13 13:04:44 +0530

    Add files via upload

---
+++ 17_loop.c
@@ -0,0 +1,20 @@
+/* THE write a program to write number from 1,2,3,4..............100 in is sequence
+   1 creat the veriable in nub
+   2 then diplay the frist nub
+   3  nub = nub + 1 
+   diplay the nub
+   diplay the nice try
+
+*/
+ #include<stdio.h>
+ void main ()
+ {
+        int number = 1;
+    while(number <= 100)  
+    {
+        printf("%d ", number); 
+        number = number + 1; 
+    }
+    printf("\n nice");
+}
+ 
