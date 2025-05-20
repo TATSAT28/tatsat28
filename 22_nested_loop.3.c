825bd658846fc152fbfcc369610e925778d5faa9
TATSAT28 <tatsat28@gmail.com>
2025-05-20 12:01:42 +0530

    Add files via upload

---
+++ 22_nested_loop.3.c
@@ -0,0 +1,21 @@
+/* write a program to display below pattern (only one * in one printf statement)
+* * * * *
+* * * *
+* * *
+* * 
+*
+
+*/
+# include<stdio.h>
+void main()
+{
+    int i,j;
+    for(j=1;j<=5;j++) 
+    {
+        for (i = 5; i >= j; i--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
